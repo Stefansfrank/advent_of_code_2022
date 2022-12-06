6201c53bcf50a7343437acad330108d443d11c94
Stefan Frank <62906492+Stefansfrank@users.noreply.github.com>
2022-12-06 09:08:32 -0500

    Better version of Day 6

--- Day6.kt
+++ Day6.kt
@@ -6,26 +6,17 @@ class Day6 : Solver {
 
     override fun solve(file: String) {
 
-        // parsing input
+        // reading input
         val data = readTxtFile(file)[0].toList()
 
-        // Part 1 using windowing and
-        // the length of the window converted to a set
-        val wdata1 = data.windowed(4)
-        for (ix in wdata1.indices) {
-            if (wdata1[ix].toSet().size == 4) {
-                println("\nPart 1: $red$bold${ix+4}$reset")
-                break
-            }
-        }
-
-        // Part 2 using windowing and
-        // the length of the window converted to a set
-        val wdata2 = data.windowed(14)
-        for (ix in wdata1.indices) {
-            if (wdata2[ix].toSet().size == 14) {
-                println("Part 2: $red$bold${ix+14}$reset")
-                break
+        // using windowing and
+        // the length of the corresponding set
+        for ((part, len) in listOf(4,14).withIndex()) {
+            for ((ix, window) in data.windowed(len).withIndex()) {
+                if (window.toSet().size == len) {
+                    println("Part ${part + 1}: $red$bold${ix + len}$reset")
+                    break
+                }
             }
         }
     }
